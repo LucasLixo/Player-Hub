127dce9735532a6444e56f3df2c49ce144e3472b
LucasLixo <paulolucasalves2006@gmail.com>
2024-09-08 14:27:41 -0300

    native-debug-symbols

--- native-debug-symbols.bat
+++ native-debug-symbols.bat
@@ -4,11 +4,11 @@ set "source_dir=.\build\app\intermediates\merged_native_libs\release\out\lib"
 set "output_zip=.\native-debug-symbols.zip"
 
 if exist "%output_zip%" (
-    echo Excluindo arquivo existente: %output_zip%
+    echo %output_zip%
     del /f /q "%output_zip%"
 )
 
-echo Compactando %source_dir% em %output_zip%
+echo %source_dir% - %output_zip%
 powershell -command "Compress-Archive -Path '%source_dir%\*' -DestinationPath '%output_zip%'"
 
 if exist "%output_zip%" (
@@ -16,5 +16,3 @@ if exist "%output_zip%" (
 ) else (
     echo Error.
 )
-
-pause
